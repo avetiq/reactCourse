03667e54580fbf1e71d5d7f6dfb4f239090c0e2e
avetiq <avo6385@gmail.com>
2020-12-19 18:59:48 +0400

    ComponentFunctions

---
+++ src/Address.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function Address(props){
+    return <p>I live in {props.city}, {props.country}</p>
+}
+
---
+++ src/Title.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export function Title(props){
+    return <h2>{props.name}</h2>
+}
+
